e3a764a41342278ceca33780baa4d529ae279a23
Daniel Holanda <danielholanda55@gmail.com>
2026-05-20 22:29:29 -0300

    configuração inicial do projeto Next.js 14

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({ dir: './' })
+
+/** @type {import('jest').Config} */
+const config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/@types/**',
+  ],
+}
+
+module.exports = createJestConfig(config)
